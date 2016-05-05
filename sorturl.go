07969b3cc2874b2f6b209fc129e47896b9e3ad8c
caoxiaolin <xmihu945@qq.com>
2016-05-05 18:44:24 +0800

    modify by caoxiaolin

--- sorturl.go
+++ sorturl.go
@@ -4,11 +4,12 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "database/sql"
-    "./utils/"
+	"./utils/"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
 const host = "192.168.245.128:4000"
@@ -18,31 +19,35 @@ type Sorturl struct{}
 var db *sql.DB
 
 func init() {
-    dsn := "root:123456@tcp(192.168.245.128:3550)/sorturl?charset=utf8"
-    db, _ = sql.Open("mysql", dsn)
-    db.SetMaxOpenConns(2000)
-    db.SetMaxIdleConns(1000)
-    db.Ping()
+	dsn := "root:123456@tcp(192.168.245.128:3550)/sorturl?charset=utf8"
+	db, _ = sql.Open("mysql", dsn)
+	db.SetMaxOpenConns(2000)
+	db.SetMaxIdleConns(1000)
+	db.Ping()
 }
 
 func (this Sorturl) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    method := r.Method
-    if method == "GET" { 
-        uri := r.RequestURI
-        l := len(uri)
-        fmt.Fprint(w, utils.GetOriUrl(db, uri[1:l]))
-        fmt.Fprint(w, "\n")
-    } else if method == "POST" {
-        r.ParseForm()
-        fmt.Fprint(w, "http://" + host + "/" + utils.GetSortUrl(db, r.Form["url"][0]))
-        fmt.Fprint(w, "\n")
-    }
+	method := r.Method
+	if method == "GET" {
+		uri := r.RequestURI
+		l := len(uri)
+		res := utils.GetOriUrl(db, uri[1:l])
+		fmt.Println("[" + time.Now().Format("2006/01/02 15:04:05") + "] [GET] [" + uri + "] [" + res + "]")
+		fmt.Fprint(w, res)
+		fmt.Fprint(w, "\n")
+	} else if method == "POST" {
+		r.ParseForm()
+		res := utils.GetSortUrl(db, r.Form["url"][0])
+		fmt.Println("[" + time.Now().Format("2006/01/02 15:04:05") + "] [POST] [" + r.Form["url"][0] + "] [" + res + "]")
+		fmt.Fprint(w, "http://"+host+"/"+res)
+		fmt.Fprint(w, "\n")
+	}
 }
 
 func main() {
-    var s Sorturl
-    err := http.ListenAndServe(host, s)
-    if err != nil {
-        log.Fatal(err)
-    }
+	var s Sorturl
+	err := http.ListenAndServe(host, s)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
