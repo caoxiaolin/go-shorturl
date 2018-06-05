9d7e7c7291cafcf82284739e077692973e4027e1
caoxiaolin <xmihu945@qq.com>
2018-06-05 23:24:33 +0800

    modify by caoxiaolin

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"github.com/BurntSushi/toml"
+	"path/filepath"
+)
+
+type TomlConfig struct {
+	Server server
+	Database database
+}
+
+type database struct {
+	Host string
+	Port int
+	UserName string
+	PassWord string
+	DbName string
+	MaxConn int
+}
+
+type server struct {
+	Host string
+	Port int
+}
+
+func Load() *TomlConfig {
+	filePath, err := filepath.Abs("config/config.toml")
+	if err != nil {
+		panic(err)
+	}
+	var config = new(TomlConfig)
+	if _, err := toml.DecodeFile(filePath, &config); err != nil {
+		panic(err)
+	}
+	return config
+}
