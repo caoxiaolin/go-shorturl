bd5bcc56812b88e2bcd8d9acf98c4c802a02c611
caoxiaolin <xmihu945@qq.com>
2018-06-05 11:24:22 +0800

    modify by caoxiaolin

--- shorturl.go
+++ shorturl.go
@@ -4,10 +4,10 @@
 package main
 
 import (
-	"github.com/caoxiaolin/go-shorturl/utils"
 	"database/sql"
-    _ "github.com/lib/pq"
 	"fmt"
+	"github.com/caoxiaolin/go-shorturl/utils"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 )
@@ -15,11 +15,11 @@ import (
 const service = "127.0.0.1:4000"
 
 const (
-    dbhost      = "127.0.0.1"
-    dbport      = 5432
-    username    = "rdtest"
-    password    = "123456"
-    dbname      = "shorturl"
+	dbhost   = "127.0.0.1"
+	dbport   = 5432
+	username = "rdtest"
+	password = "123456"
+	dbname   = "shorturl"
 )
 
 type Shorturl struct{}
@@ -27,8 +27,10 @@ type Shorturl struct{}
 var db *sql.DB
 
 func init() {
-    dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", dbhost, dbport, username, password, dbname)
-    db, _ = sql.Open("postgres", dsn)
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", dbhost, dbport, username, password, dbname)
+	db, _ = sql.Open("postgres", dsn)
+	db.SetMaxOpenConns(2000)
+	db.SetMaxIdleConns(1000)
 	db.Ping()
 }
 
@@ -38,11 +40,13 @@ func (this Shorturl) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		uri := r.RequestURI
 		l := len(uri)
 		res := utils.GetOriUrl(db, uri[1:l])
-        log.Printf("[GET] [%s] [%s]", uri, res)
+		fmt.Fprintln(w, res)
+		log.Printf("[GET] [%s] [%s]", uri, res)
 	} else if method == "POST" {
 		r.ParseForm()
 		res := utils.GetShortUrl(db, r.Form["url"][0])
-        log.Printf("[POST] [%s] [%s]", r.Form["url"][0], res)
+		fmt.Fprintln(w, "http://"+service+"/"+res)
+		log.Printf("[POST] [%s] [%s]", r.Form["url"][0], res)
 	}
 }
 
--- utils/utils.go
+++ utils/utils.go
@@ -7,88 +7,85 @@
 package utils
 
 import (
-    "strconv"
-    "math"
-	"log"
-    "database/sql"
-    _ "github.com/lib/pq"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"math"
+	"strconv"
 )
 
 //单个字符转数字，传入ascii
-func Str2int(i rune) int64{
-    var res  int32
-     if i >= 48 && i <= 57 {
-        res = i - 48
-    } else if i >= 65 && i <= 90 {
-        res = i - 65 + 10
-    } else {
-        res = i - 97 + 36
-    }
-    return int64(res)
+func Str2int(i rune) int64 {
+	var res int32
+	if i >= 48 && i <= 57 {
+		res = i - 48
+	} else if i >= 65 && i <= 90 {
+		res = i - 65 + 10
+	} else {
+		res = i - 97 + 36
+	}
+	return int64(res)
 }
 
 //数字转字符
 func Int2str(i int) string {
-    var res string
-    if i >=0 && i <= 9 {
-        res =  strconv.Itoa(int(i))
-    } else if i >= 10 && i <= 35 {
-        res = string(i - 10 + 'A')
-    } else {
-        res = string(i - 36 + 'a')
-    }
-    return res
+	var res string
+	if i >= 0 && i <= 9 {
+		res = strconv.Itoa(int(i))
+	} else if i >= 10 && i <= 35 {
+		res = string(i - 10 + 'A')
+	} else {
+		res = string(i - 36 + 'a')
+	}
+	return res
 }
 
 //10进制转62进制
 func Convert_10_to_62(num int) string {
-    var res string
-    var ys int
-    s := num
-    for s >= 1 {
-        tmp := s/62
-        ys = s - tmp * 62
-        s = tmp
-        res = Int2str(ys) + res
-    }
-    return res
+	var res string
+	var ys int
+	s := num
+	for s >= 1 {
+		tmp := s / 62
+		ys = s - tmp*62
+		s = tmp
+		res = Int2str(ys) + res
+	}
+	return res
 }
 
 //62进制转10进制
 func Convert_62_to_10(str string) int64 {
-    var res int64 = 0
-    len := len(str)
-    for k, v := range str {
-        res = res + Str2int(v) * int64(math.Pow(float64(62), float64(len - 1 - k)))
-    }
-    return res
+	var res int64 = 0
+	len := len(str)
+	for k, v := range str {
+		res = res + Str2int(v)*int64(math.Pow(float64(62), float64(len-1-k)))
+	}
+	return res
 }
 
 //获取短链接
 func GetShortUrl(db *sql.DB, url string) string {
-	sql := "INSERT INTO url (url, hits, create_time) VALUES ('$1', 0, CURRENT_TIMESTAMP) RETURNING id"
-    err := db.QueryRow(sql, url).Scan(&id)
-    if err != nil {
-        panic(err)
-    }
-    return Convert_10_to_62(int(id))
+	var id int
+	sql := `INSERT INTO url (url, hits, create_time) VALUES ($1, 0, CURRENT_TIMESTAMP) RETURNING id`
+	err := db.QueryRow(sql, url).Scan(&id)
+	if err != nil {
+		panic(err)
+	}
+	return Convert_10_to_62(int(id))
 }
 
 //获取原始链接
 func GetOriUrl(db *sql.DB, url string) string {
-    id := Convert_62_to_10(url)
-    rows, _ := db.Query("SELECT url FROM url WHERE id = ?", id)
-    var oriurl string
-    for rows.Next() {
-        err := rows.Scan(&oriurl)
-        if err != nil {
-            panic(err)
-        }
-        stmt, _ := db.Prepare("UPDATE url SET hits = hits + 1, last_access_time = NOW() WHERE id = ?")
-        stmt.Exec(id)
-    }
-
-    defer rows.Close()
-
-    return oriurl
+	var oriurl string
+	id := Convert_62_to_10(url)
+	sql := `SELECT url FROM url WHERE id = $1;`
+	err := db.QueryRow(sql, id).Scan(&oriurl)
+	switch err {
+	//case sql.ErrNoRows:
+	case nil:
+		//stmt, _ := db.Prepare("UPDATE url SET hits = hits + 1, last_access_time = NOW() WHERE id = ?")
+	default:
+		panic(err)
+	}
+	return oriurl
 }
