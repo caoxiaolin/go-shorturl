40cd57e9b512db3efdb580d6a637a9b99ac6e597
caoxiaolin <xmihu945@qq.com>
2018-07-17 10:40:58 +0800

    modify by caoxiaolin

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -10,7 +12,7 @@ import (
 func TestConvert_10_to_62(t *testing.T) {
 	ret := Convert_10_to_62(10000)
 	if ret != "2bI" {
-		t.Error("Expected 2bI, but got ", ret)
+		t.Error("Expected 2bI, but got", ret)
 	}
 }
 
@@ -20,6 +22,18 @@ func TestConvert_10_to_62(t *testing.T) {
 func TestConvert_62_to_10(t *testing.T) {
 	ret := Convert_62_to_10("2bI")
 	if ret != 10000 {
-		t.Error("Expected 10000, but got ", ret)
+		t.Error("Expected 10000, but got", ret)
+	}
+}
+
+/**
+ * 测试从post request获取url参数
+ */
+func TestGetPostUrl(t *testing.T) {
+	req := httptest.NewRequest("POST", "/", strings.NewReader("url=http://www.google.com"))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	url := GetPostUrl(req)
+	if url != "http://www.google.com" {
+		t.Error("Expected http://www.google.com, but got", url)
 	}
 }
