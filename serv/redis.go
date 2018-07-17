f576cf4ba792ec677e0b6833924f4e2a2e49a5b7
caoxiaolin <xmihu945@qq.com>
2018-07-17 18:19:12 +0800

    modify by caoxiaolin

--- serv/redis.go
+++ serv/redis.go
@@ -10,22 +10,30 @@ import (
 var Rds redis.Conn
 
 func init() {
+	c, err := Conn()
+	if err != nil {
+		panic(err)
+	}
+	Rds = c
+}
+
+func Conn() (redis.Conn, error) {
 	host := fmt.Sprintf("%s:%d", config.Cfg.Redis.Host, config.Cfg.Redis.Port)
 	c, err := redis.Dial("tcp", host)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	if config.Cfg.Redis.Password != "" {
 		if _, err := c.Do("AUTH", config.Cfg.Redis.Password); err != nil {
 			c.Close()
-			panic(err)
+			return nil, err
 		}
 	}
 	if config.Cfg.Redis.Database > 0 {
 		if _, err := c.Do("SELECT", config.Cfg.Redis.Database); err != nil {
 			c.Close()
-			panic(err)
+			return nil, err
 		}
 	}
-	Rds = c
+	return c, nil
 }
--- serv/redis_test.go
+++ serv/redis_test.go
@@ -1,14 +1,37 @@
 package serv
 
 import (
+	"github.com/caoxiaolin/go-shorturl/config"
+	"github.com/gomodule/redigo/redis"
 	"testing"
-    "github.com/gomodule/redigo/redis"
 )
 
-func TestRedis(t *testing.T){
-    rdskey := "test_redis"
-    Rds.Do("SET", rdskey, "this is a test")
-    if rdsval, _ := redis.String(Rds.Do("GET", rdskey)); rdsval != "this is a test" {
-        t.Error("Expected this is a test, but got", rdsval)
-    }
+func TestRedis(t *testing.T) {
+	rdskey := "test_redis"
+	Rds.Do("SET", rdskey, "this is a test")
+	if rdsval, _ := redis.String(Rds.Do("GET", rdskey)); rdsval != "this is a test" {
+		t.Error("Expected this is a test, but got", rdsval)
+	}
+}
+
+func TestConn(t *testing.T) {
+	config.Cfg.Redis.Port = 1000
+	_, err := Conn()
+	if err == nil {
+		t.Error(err)
+	}
+
+	config.Cfg.Redis.Port = 6379
+	config.Cfg.Redis.Password = "123456"
+	_, err = Conn()
+	if err == nil {
+		t.Error(err)
+	}
+
+	config.Cfg.Redis.Password = ""
+	config.Cfg.Redis.Database = 100
+	_, err = Conn()
+	if err == nil {
+		t.Error(err)
+	}
 }
